3a8861c17152bb63925eacfd34326beed489b590
rm-ffx <robert@freshfx.at>
2016-05-24 17:31:53 +0200

    added FadeInOut Script -> work in progress!

---
+++ Assets/Scripts/FadeInOutWarning.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeInOutWarning : MonoBehaviour {
+
+    private Texture m_warning;
+    public float FadeSpeed = 1.0f;
+    public Renderer Warning;
+
+	// Use this for initialization
+	void Start () {
+        Warning = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void FadeIn()
+    {
+        
+    }
+
+    void FadeOut()
+    {
+
+    }
+}
