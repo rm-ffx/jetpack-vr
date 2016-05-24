89a31e7fa9756342c4a2ffee4439a7e4a794e63b
tk-ffx <tony@freshfx.at>
2016-05-24 15:23:11 +0200

    added a Levelborder and PlayerFallDownTrigger to prevent the player to fly to high or to fall down

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOver : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "[CameraRig]")
+        {
+            Debug.Log("Game Over");
+        }
+    }
+}
