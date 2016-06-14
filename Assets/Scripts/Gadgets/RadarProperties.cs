ff4bb53e815b86655c69edaba43fd2ff550692b5
Lucas Weitgasser <lucas@freshfx.at>
2016-06-14 13:18:14 +0200

    @robert - look at this lines of code please & rework to work with multiple radar objects/materials. (It's just about the texture scaling, more information in code comments)

--- Assets/Scripts/Gadgets/RadarProperties.cs
+++ Assets/Scripts/Gadgets/RadarProperties.cs
@@ -17,7 +17,6 @@ public class RadarProperties : MonoBehaviour
     [Tooltip("Radius of the radar.")]
     public float switchDistance;
 
-    private Material radarMaterial;
 
     public List<GameObject> radarObjects { get; private set; }
     public List<Renderer> radarRenderers { get; private set; }
@@ -28,7 +27,9 @@ public class RadarProperties : MonoBehaviour
         if (trackedObjects.Length > 0)
             createRadarObjects();
 
-        radarMaterial = radarPrefabs[0].GetComponentInChildren<Renderer>().sharedMaterial;
+        // Not quite sure how to rework this to work on multiple radarobjects / multiple materials or why it's even needed.
+        // Might be best to scale the texture manually and not in the code, since it would be ugly to hardcode all material scalings and I'm not sure if there's a generic solution (at least no performant one)
+        Material radarMaterial = defaultRadarPrefab.GetComponentInChildren<Renderer>().sharedMaterial;
         radarMaterial.mainTextureScale = new Vector2(0.7f, 0.7f);  // for scaling the icon on the GameObject -> 0.7 best value for this icon texture 
 
         foreach (Radar radar in GetComponentsInChildren<Radar>())
